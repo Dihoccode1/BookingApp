9f69b78e5c455c0f15f648bb9f9e443595767b42
Khanhduy <khdidenroiday@gmail.com>
2026-03-30 15:00:43 +0700

    complete navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,115 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { assets } from "../assets/assets";
-import { MenuIcon, SearchIcon, XIcon } from "lucide-react";
-
+import {
+  MenuIcon,
+  SearchIcon,
+  TicketCheckIcon,
+  TicketPlus,
+  User,
+  UserCheck,
+  XIcon,
+} from "lucide-react";
+import { useUser } from "@clerk/react";
+import {
+  Show,
+  SignInButton,
+  SignUpButton,
+  useClerk,
+  UserButton,
+} from "@clerk/react";
 const Navbar = () => {
+  // State quản lý việc đóng/mở menu trên mobile
+  const [isOpen, setIsOpen] = useState(false);
+  const { user } = useUser();
+  const { openSignIn } = useClerk();
+  const navigate = useNavigate();
   return (
-    <div className="fixed top-0 left-0 right-0 z-50 w-full flex items-center justify-between px-6 md:px-16 lg:px-36 py-5">
+    <div className="fixed top-0 right-0 left-0 z-50 flex w-full items-center justify-between px-6 py-5 md:px-16 lg:px-36">
       <Link to="/" className="max-md:flex-1">
-        <img src={assets.logo} alt="Logo" className="w-36 h-auto" />
+        <img src={assets.logo} alt="Logo" className="h-auto w-36" />
       </Link>
-      {/* //Menu list */}
-      <div className="max-md:abolute max-md:top-0 max-md:left-0 max-md:font-medium max-md:text-lg z-50 flex flex-col md:flex-row items-center gap-6 md:gap-10 bg-black/90 backdrop-blur-sm md:bg-transparent md:backdrop-blur-0 w-full md:w-auto h-screen md:h-auto justify-center items-center max-md:justify-center gap-8 min-md:px-8 py-3 max-md:h-screen min-md:rounded-full backdrop-blur bg-black/70 mg:bg-white/10 md:border border-gray-300/20 overflow-hidden transition-[width] duration-300">
-        <XIcon className="md:hidden absolute top-6 right-6 w-6 h-6 cursor-pointer" />
-        <Link to="/">Home</Link>
-        <Link to="/movies">Movies</Link>
-        <Link to="/">Releases</Link>
-        <Link to="/">Theaters</Link>
+
+      {/* Menu list */}
+      <div
+        className={`md:backdrop-blur-0 z-50 flex flex-col items-center justify-center gap-8 overflow-hidden border-gray-300/20 bg-black/90 py-3 backdrop-blur-sm transition-[width] duration-300 max-md:absolute max-md:top-0 max-md:left-0 max-md:h-screen max-md:text-lg max-md:font-medium md:h-auto md:w-auto md:flex-row md:gap-10 md:rounded-full md:border md:bg-transparent md:px-8 ${
+          isOpen ? "max-md:w-full" : "max-md:w-0"
+        }`}
+      >
+        <XIcon
+          className="absolute top-6 right-6 h-6 w-6 cursor-pointer text-white md:hidden"
+          onClick={() => {
+            setIsOpen(!isOpen);
+          }}
+        />
+        <Link
+          to="/"
+          onClick={() => {
+            setIsOpen(false);
+            scrollTo(0, 0);
+          }}
+          className="hover:text-primary text-white"
+        >
+          Home
+        </Link>
+        <Link
+          to="/movies"
+          onClick={() => {
+            setIsOpen(false);
+            scrollTo(0, 0);
+          }}
+          className="hover:text-primary text-white"
+        >
+          Movies
+        </Link>
+        <Link
+          to="/"
+          onClick={() => {
+            setIsOpen(false);
+            scrollTo(0, 0);
+          }}
+          className="hover:text-primary text-white"
+        >
+          Releases
+        </Link>
+        <Link
+          to="/"
+          onClick={() => {
+            setIsOpen(false);
+            scrollTo(0, 0);
+          }}
+          className="hover:text-primary text-white"
+        >
+          Theaters
+        </Link>
       </div>
-      {/* //Login and Search icon */}
+
+      {/* Login and Search icon */}
       <div className="flex items-center gap-8">
-        <SearchIcon className="max-md:hidden w-6 h-6 cursor-pointer   " />
-        <button className="px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer">
-          Login
-        </button>
+        <SearchIcon className="h-6 w-6 cursor-pointer max-md:hidden" />
+        {!user ? (
+          <button
+            onClick={openSignIn}
+            className="bg-primary hover:bg-primary-dull cursor-pointer rounded-full px-4 py-1 font-medium text-white transition sm:px-7 sm:py-2"
+          >
+            Login
+          </button>
+        ) : (
+          <UserButton>
+            <UserButton.MenuItems>
+              <UserButton.Action
+                label="My Bookings"
+                labelIcon={<TicketPlus width={15} />}
+                onClick={() => navigate("/my-bookings")}
+              />
+            </UserButton.MenuItems>
+          </UserButton>
+        )}
+        <MenuIcon
+          className="h-8 w-8 cursor-pointer max-md:ml-4 md:hidden"
+          onClick={() => setIsOpen(!isOpen)}
+        />
       </div>
-      {/* //Menu icon */}
-      <MenuIcon className="max-md:ml-4 md:hidden w-8 h-8 cursor-pointer" />
     </div>
   );
 };
