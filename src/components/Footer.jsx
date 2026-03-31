f18554a97e1b2922ed2f3f7566018cac231a9bd8
Khanhduy <khdidenroiday@gmail.com>
2026-03-31 16:34:16 +0700

    Complete Movies

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,48 @@
 import React from "react";
-
+import logo from "../assets/logo.svg";
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <>
+      <footer className="w-full px-6 pt-8 text-gray-300 md:px-16 lg:px-36">
+        <div className="flex w-full flex-col justify-between gap-10 border-b border-gray-500 pb-10 md:flex-row">
+          <div className="md:max-w-96">
+            <img alt="" class="h-11" src={logo} />
+            <p className="mt-6 text-sm">
+              Đây là nền tảng đặt vé xem phim trực tuyến, giúp bạn dễ dàng lựa
+              chọn phim, suất chiếu và rạp phù hợp. Trải nghiệm đặt vé nhanh
+              chóng, tiện lợi và hiện đại ngay tại nhà.
+            </p>
+          </div>
+          <div className="flex flex-1 items-start gap-20 md:justify-end md:gap-40">
+            <div>
+              <h2 className="mb-5 font-semibold">Company</h2>
+              <ul className="space-y-2 text-sm">
+                <li>
+                  <a href="#">Trang chủ</a>
+                </li>
+                <li>
+                  <a href="#">Giới thiệu</a>
+                </li>
+                <li>
+                  <a href="#">Liên hệ</a>
+                </li>
+                <li>
+                  <a href="#">Chính sách bảo mật</a>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h2 className="mb-5 font-semibold">Get in touch</h2>
+              <div className="space-y-2 text-sm">
+                <p>+84-833-866-228</p>
+                <p>khdidenroiday@gmail.com</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
 };
 
 export default Footer;
--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,7 +1,26 @@
 import React from "react";
-
+import { dummyShowsData } from "../assets/assets";
+import MovieCard from "../components/MovieCard";
+import BlurCircle from "../components/BlurCircle";
 const Movies = () => {
-  return <div>Movies</div>;
+  return dummyShowsData.length > 0 ? (
+    <div className="relative my-40 mb-60 min-h-[80vh] overflow-hidden px-6 md:px-16 lg:px-40 xl:px-44">
+      <BlurCircle top="150px" left="0" />
+      <BlurCircle bottom="150px" right="50px" />
+      <h1>Phim đang chiếu</h1>
+      <div className="flex flex-wrap gap-8 max-sm:justify-center">
+        {dummyShowsData.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </div>
+    </div>
+  ) : (
+    <div className="flex h-screen flex-col items-center justify-center">
+      <h1 className="text-center text-3xl font-bold">
+        Hiện chưa có phim nào đang chiếu
+      </h1>
+    </div>
+  );
 };
 
 export default Movies;
